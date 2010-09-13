bac1c7e5d7bdb383ec0bf2d4e1bddd1c54e6e210
ado <ado@ado-laptop.(none)>
2010-09-12 23:10:39 -0300

    Experimentos com o CRUD para form

--- controllers/eval.py
+++ controllers/eval.py
@@ -1,5 +1,12 @@
 from gluon.tools import Crud
 crud = Crud(globals(), db)
 
+#request.vars.professor_id
+
 def aval_add():
-	return dict(form_add=crud.create(db.avaliacoes), message=("Avaliação realizada com sucesso"))
+	form_add=crud.create(db.avaliacoes, message="Avaliação realizada com sucesso", next=URL('aval_list'))
+	return dict(form_add=form_add)
+
+def aval_list():
+	avals=crud.select(db.avaliacoes, db.avaliacoes.professor_id==1)
+	return dict(avals=avals)
