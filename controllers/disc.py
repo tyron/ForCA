95927239b65cdec5238002c317b2b0b7e3c43c79
Tiago <tiago.strato@gmail.com>
2010-09-23 22:53:57 -0300

    Mudancas em prof/home para compatibilizar com GAE

--- controllers/disc.py
+++ controllers/disc.py
@@ -1,3 +1,10 @@
+@auth.requires_login()
+def create():
+	form = SQLFORM(db.disciplinas)
+	if form.accepts(request.vars, session):
+		session.flash = 'ok'
+	return dict(form=form)
+
 def list():
 	'''
 	Exibe a lista de disciplinas
--- controllers/prof.py
+++ controllers/prof.py
@@ -1,5 +1,12 @@
 #profs controller
 
+@auth.requires_login()
+def create():
+	form = SQLFORM(db.professores)
+	if form.accepts(request.vars, session):
+		session.flash = 'ok'
+	return dict(form=form)
+
 def list():
 	'''
 	Exibe a lista de professores
@@ -17,8 +24,8 @@ def home():
 	for raw_eval in raw_evals:
 		eval = {}
 		eval['id']       = raw_eval['id']
-		eval['aluno']    = db(db.alunos.id==raw_eval['aluno_id']).select().first()
-		eval['disc']     = db(db.disciplinas.id==raw_eval['disciplina_id']).select().first()
+		eval['aluno_name']    = db(db.alunos.id==raw_eval['aluno_id']).select().first().full_name
+		eval['disc_name']     = db(db.disciplinas.id==raw_eval['disciplina_id']).select().first().name
 		eval['semester'] = str(raw_eval['year'])+'/'+str(raw_eval['semester'])
 		eval['grade']    = raw_eval['grade']
 		eval['comment']  = raw_eval['comment']
