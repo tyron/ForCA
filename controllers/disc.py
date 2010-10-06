c90e414f0972f4b366242e8d7cf765c99e883ffc
unknown <Thomas@.(none)>
2010-10-05 23:09:54 -0300

    Melhorado o home da disciplina, agora com sumula e professores que ja lecionaram.

--- controllers/disc.py
+++ controllers/disc.py
@@ -2,10 +2,10 @@ from operator import itemgetter
 
 @auth.requires_login()
 def create():
-	form = SQLFORM(db.disciplinas)
-	if form.accepts(request.vars, session):
-		session.flash = 'ok'
-	return dict(form=form)
+    form = SQLFORM(db.disciplinas)
+    if form.accepts(request.vars, session):
+        session.flash = 'ok'
+    return dict(form=form)
 
 def list():
     '''
@@ -14,23 +14,36 @@ def list():
     return dict(discs=db().select(db.disciplinas.ALL).sort(lambda discs: discs.name))
     
 def home():
-	'''
-	Lista avaliações recebidas pela disciplina
-	'''
-	disc_id = request.vars['disc_id']
-	disc = db(db.disciplinas.id==disc_id).select(db.disciplinas.ALL).first()
-	raw_evals = db(db.avaliacoes.disciplina_id==disc_id).select()
-	evals = []
-	for raw_eval in raw_evals:
-		eval = {}
-		eval['id']            = raw_eval['id']
-		eval['aluno_user_id'] = db(db.alunos.id==raw_eval['aluno_id']).select().first().user_id
-		eval['aluno_id']      = raw_eval['aluno_id']
-		eval['aluno_name']    = db(db.alunos.id==raw_eval['aluno_id']).select().first().full_name
-		eval['prof_name']     = db(db.professores.id==raw_eval['professor_id']).select().first().full_name
-		eval['semester']      = str(raw_eval['year'])+'/'+str(raw_eval['semester'])
-		eval['grade']         = raw_eval['grade']
-		eval['karma']         = raw_eval['karma']
-		eval['comment']       = raw_eval['comment']
-		evals.append(eval)
-	return dict(disc = disc, evals = sorted(evals, key=itemgetter('karma'), reverse=True))
+    '''
+    Lista avaliações recebidas pela disciplina
+    '''
+    disc_id = request.vars['disc_id']
+    disc = db(db.disciplinas.id==disc_id).select(db.disciplinas.ALL).first()
+    #Lista de avaliações
+    raw_evals = db(db.avaliacoes.disciplina_id==disc_id).select()
+    evals = []
+    for raw_eval in raw_evals:
+        eval = {}
+        eval['id']            = raw_eval['id']
+        eval['aluno_user_id'] = db(db.alunos.id==raw_eval['aluno_id']).select().first().user_id
+        eval['aluno_id']      = raw_eval['aluno_id']
+        eval['aluno_name']    = db(db.alunos.id==raw_eval['aluno_id']).select().first().full_name
+        eval['prof_name']     = db(db.professores.id==raw_eval['professor_id']).select().first().full_name
+        eval['semester']      = str(raw_eval['year'])+'/'+str(raw_eval['semester'])
+        eval['grade']         = raw_eval['grade']
+        eval['karma']         = raw_eval['karma']
+        eval['comment']       = raw_eval['comment']
+        evals.append(eval)
+    #Lista de professores que dão a disciplina
+    raw_profs = db(db.profs_discs.disciplina_id==disc_id).select()
+    profs = []
+    for raw_prof in raw_profs:
+        prof = {}
+        prof['id']        = raw_prof['professor_id']
+        prof['full_name'] = db(db.professores.id==raw_prof['professor_id']).select().first().full_name
+        evals_prof_disc   = db((db.avaliacoes.disciplina_id==disc_id)&(db.avaliacoes.professor_id==raw_prof['professor_id']))
+        prof['grade']     = grade_average(evals_prof_disc) 
+        profs.append(prof)
+         
+        
+    return dict(disc = disc, evals = sorted(evals, key=itemgetter('karma'), reverse=True), profs = sorted(profs, key=itemgetter('grade'), reverse=False))
--- models/models.py
+++ models/models.py
@@ -1,103 +1,104 @@
 from datetime import datetime
 
 if request.env.web2py_runtime_gae:
-	db = DAL('gae')
-	session.connect(request, response, db)
+    db = DAL('gae')
+    session.connect(request, response, db)
 else:
-	db = DAL('postgres://forca:xx@localhost/forca')
+    db = DAL('postgres://forca:xx@localhost/forca')
 
 #Tabela Alunos
 db.define_table(
-		'alunos',
-		Field('user_id', 'integer', length=32),
-		Field('email', 'string', length=64, required=True, notnull=True, unique=True,
-			requires = IS_EMAIL()),
-		Field('full_name', 'string', length=128, required=True, notnull=True),
-		Field('short_name', 'string', length=32),
-		Field('grade', 'string', length=1, writable=False, readable=False,
-			requires = IS_IN_SET(['A', 'B', 'C', 'D', 'FF'])),
-		Field('picture', 'upload'),
-		migrate='alunos.table')
+        'alunos',
+        Field('user_id', 'integer', length=32),
+        Field('email', 'string', length=64, required=True, notnull=True, unique=True,
+            requires = IS_EMAIL()),
+        Field('full_name', 'string', length=128, required=True, notnull=True),
+        Field('short_name', 'string', length=32),
+        Field('grade', 'string', length=1, writable=False, readable=False,
+            requires = IS_IN_SET(['A', 'B', 'C', 'D', 'FF'])),
+        Field('picture', 'upload'),
+        migrate='alunos.table')
 
 #Tabela Disciplinas
 db.define_table(
-		'disciplinas',
-		Field('name', 'string', length=128, required=True, notnull=True),
-		Field('short_name', 'string', length=32),
-		Field('code', 'string', length=8, required=True, notnull=True, unique=True),
-		migrate='disciplinas.table')
+        'disciplinas',
+        Field('name', 'string', length=128, required=True, notnull=True),
+        Field('short_name', 'string', length=32),
+        Field('code', 'string', length=8, required=True, notnull=True, unique=True),
+        Field('resume', 'text'),
+        migrate='disciplinas.table')
 
 #Tabela Professores
 db.define_table(
-		'professores',
-		Field('user_id', 'integer', length=32),
-		Field('email', 'string', length=64, required=True, notnull=True, unique=True,
-			requires = IS_EMAIL()),
-		Field('full_name', 'string', length=128, required=True, notnull=True),
-		Field('short_name', 'string', length=32),
-		Field('grade', 'string', length=2, writable=False, readable=False,
-			requires = IS_IN_SET(['A', 'B', 'C', 'D', 'FF'], zero=None)),
-		Field('picture', 'upload'),
-		migrate='professores.table')
+        'professores',
+        Field('user_id', 'integer', length=32),
+        Field('email', 'string', length=64, required=True, notnull=True, unique=True,
+            requires = IS_EMAIL()),
+        Field('full_name', 'string', length=128, required=True, notnull=True),
+        Field('short_name', 'string', length=32),
+        Field('grade', 'string', length=2, writable=False, readable=False,
+            requires = IS_IN_SET(['A', 'B', 'C', 'D', 'FF'], zero=None)),
+        Field('picture', 'upload'),
+        migrate='professores.table')
 
 #Tabela Avaliacoes
 db.define_table(
-		'avaliacoes',
-		Field('aluno_id', db.alunos, required=True, notnull=True,
-			writable = False, readable = False),
-		Field('disciplina_id', db.disciplinas, required=True, notnull=True,
-			readable = False,
-			requires = IS_IN_DB(db, db.disciplinas.id, '%(name)s')),
-		Field('professor_id', db.professores, required=True, notnull=True,
-			readable = False,
-			requires = IS_IN_DB(db, db.professores.id, '%(full_name)s')),
-		Field('year', 'integer', length=4, default=datetime.now().year, 
-			requires = IS_IN_SET(range(1990, datetime.now().year+1), zero=None)),
-		Field('semester', 'integer', length=1, default=(1 if datetime.now().month <= 6 else 2),
-			requires = IS_IN_SET(['1', '2'], zero=None)),
-		Field('grade', 'string', length=2, readable=False,
-			requires = IS_IN_SET(['A', 'B', 'C', 'D', 'FF'], zero=None)),
-		Field('comment', 'text'),
-		Field('karma', 'integer', length=8, default='0', writable=False, readable=False),
-		Field('reply', 'text', writable=False, readable=False),
-		migrate='avaliacoes.table')
+        'avaliacoes',
+        Field('aluno_id', db.alunos, required=True, notnull=True,
+            writable = False, readable = False),
+        Field('disciplina_id', db.disciplinas, required=True, notnull=True,
+            readable = False,
+            requires = IS_IN_DB(db, db.disciplinas.id, '%(name)s')),
+        Field('professor_id', db.professores, required=True, notnull=True,
+            readable = False,
+            requires = IS_IN_DB(db, db.professores.id, '%(full_name)s')),
+        Field('year', 'integer', length=4, default=datetime.now().year, 
+            requires = IS_IN_SET(range(1990, datetime.now().year+1), zero=None)),
+        Field('semester', 'integer', length=1, default=(1 if datetime.now().month <= 6 else 2),
+            requires = IS_IN_SET(['1', '2'], zero=None)),
+        Field('grade', 'string', length=2, readable=False,
+            requires = IS_IN_SET(['A', 'B', 'C', 'D', 'FF'], zero=None)),
+        Field('comment', 'text'),
+        Field('karma', 'integer', length=8, default='0', writable=False, readable=False),
+        Field('reply', 'text', writable=False, readable=False),
+        migrate='avaliacoes.table')
 
 db.avaliacoes.aluno_id.requires = [
-	IS_IN_DB(db, db.alunos.id, '%(full_name)s',
-		error_message = 'Aluno não cadastrado em nossa base de dados'),
-	IS_NOT_IN_DB(db(
-		(db.avaliacoes.disciplina_id == request.vars.disciplina_id) &
-		(db.avaliacoes.professor_id == request.vars.professor_id)),
-	'avaliacoes.aluno_id',
-	error_message = 'Você já postou uma avaliação para este professor nesta disciplina')
-	]
+    IS_IN_DB(db, db.alunos.id, '%(full_name)s',
+        error_message = 'Aluno não cadastrado em nossa base de dados'),
+    IS_NOT_IN_DB(db(
+        (db.avaliacoes.disciplina_id == request.vars.disciplina_id) &
+        (db.avaliacoes.professor_id == request.vars.professor_id)),
+    'avaliacoes.aluno_id',
+    error_message = 'Você já postou uma avaliação para este professor nesta disciplina')
+    ]
 
 #Tabela Prof x Disc
 db.define_table(
-		'profs_discs',
-		Field('professor_id', db.professores, required=True, notnull=True,
-			readable = False,
-			requires = IS_IN_DB(db, db.professores.id, '%(full_name)s')),
-		Field('disciplina_id', db.disciplinas, required=True, notnull=True,
-			readable = False,
-			requires = IS_IN_DB(db, db.disciplinas.id, '%(name)s')),
-			migrate='profs_discs.table')
-			
+        'profs_discs',
+        Field('professor_id', db.professores, required=True, notnull=True,
+            readable = False,
+            requires = IS_IN_DB(db, db.professores.id, '%(full_name)s')),
+        Field('disciplina_id', db.disciplinas, required=True, notnull=True,
+            readable = False,
+            requires = IS_IN_DB(db, db.disciplinas.id, '%(name)s')),
+            migrate='profs_discs.table')
+            
 db.profs_discs.professor_id.requires = [
-	IS_NOT_IN_DB(db(
-		(db.profs_discs.disciplina_id == request.vars.disciplina_id) &
-		(db.profs_discs.professor_id == request.vars.professor_id)),
-	'profs_discs.professor_id',
-	error_message = 'Voce já adiciou este professor para esta disciplina')
-	]
+    IS_NOT_IN_DB(db(
+        (db.profs_discs.disciplina_id == request.vars.disciplina_id) &
+        (db.profs_discs.professor_id == request.vars.professor_id)),
+    'profs_discs.professor_id',
+    error_message = 'Voce já adiciou este professor para esta disciplina')
+    ]
 
 #Tabela Karma
 db.define_table(
-		'karmas',
-		Field('aluno_id', db.alunos, required=True, notnull=True,
-			requires = IS_IN_DB(db, db.alunos.id, '')),
-		Field('avaliacao_id', db.avaliacoes, required=True, notnull=True,
-			requires = IS_IN_DB(db, db.avaliacoes.id, '')),
-		Field('value', 'integer', length=1,
-			requires = IS_IN_SET([-1, 1], zero=None)),
-		migrate='karmas.table')
+        'karmas',
+        Field('aluno_id', db.alunos, required=True, notnull=True,
+            requires = IS_IN_DB(db, db.alunos.id, '')),
+        Field('avaliacao_id', db.avaliacoes, required=True, notnull=True,
+            requires = IS_IN_DB(db, db.avaliacoes.id, '')),
+        Field('value', 'integer', length=1,
+            requires = IS_IN_SET([-1, 1], zero=None)),
+        migrate='karmas.table')
