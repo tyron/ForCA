5489bcf065d33c794f18357bb94b3708c1401017
Tiago <tiago.strato@gmail.com>
2010-10-14 21:06:02 -0300

    Primeira versao da pagina inicial adicionada, com controle de login

--- controllers/default.py
+++ controllers/default.py
@@ -8,47 +8,51 @@
 ## - call exposes all registered services (none by default)
 #########################################################################  
 
-@auth.requires_login()
 def index():
-    """
-    """
-    redirect(URL('profile','home'))
-    return dict()
+	"""
+	"""
+	if auth.is_logged_in():
+		redirect(URL('profile','home'))
+	else:
+		redirect(URL('default','home'))
+	return dict()
 
+def home():
+	return dict()
 
 def user():
-    """
-    exposes:
-    http://..../[app]/default/user/login 
-    http://..../[app]/default/user/logout
-    http://..../[app]/default/user/register
-    http://..../[app]/default/user/profile
-    http://..../[app]/default/user/retrieve_password
-    http://..../[app]/default/user/change_password
-    use @auth.requires_login()
-        @auth.requires_membership('group name')
-        @auth.requires_permission('read','table name',record_id)
-    to decorate functions that need access control
-    """
-    return dict(form=auth())
+	"""
+	exposes:
+	http://..../[app]/default/user/login 
+	http://..../[app]/default/user/logout
+	http://..../[app]/default/user/register
+	http://..../[app]/default/user/profile
+	http://..../[app]/default/user/retrieve_password
+	http://..../[app]/default/user/change_password
+	use @auth.requires_login()
+			@auth.requires_membership('group name')
+			@auth.requires_permission('read','table name',record_id)
+	to decorate functions that need access control
+	"""
+	return dict(form=auth())
 
 
 def download():
-    """
-    allows downloading of uploaded files
-    http://..../[app]/default/download/[filename]
-    """
-    return response.download(request,db)
+	"""
+	allows downloading of uploaded files
+	http://..../[app]/default/download/[filename]
+	"""
+	return response.download(request,db)
 
 
 def call():
-    """
-    exposes services. for example:
-    http://..../[app]/default/call/jsonrpc
-    decorate with @services.jsonrpc the functions to expose
-    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
-    """
-    session.forget()
-    return service()
+	"""
+	exposes services. for example:
+	http://..../[app]/default/call/jsonrpc
+	decorate with @services.jsonrpc the functions to expose
+	supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+	"""
+	session.forget()
+	return service()
 
 
