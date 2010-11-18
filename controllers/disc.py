777089cba6a64bc848ce1cac53d3ebb1d4711608
Tiago <tiago.strato@gmail.com>
2010-11-17 23:35:48 -0200

    adicionados filtros de busca em disc/home

--- controllers/disc.py
+++ controllers/disc.py
@@ -23,11 +23,21 @@ def home():
     else:
         page = 0
     limitby = (page*10, (page+1)*11)
+
+    result_query, defaults = get_filter_query(db(Avaliacoes.disciplina_id == disc_id))
+
+    fields = {}
+
+    fields['prof'] = get_prof_dropdown(default=defaults['prof_id'])
+    fields['year'] = get_year_dropdown(default=defaults['year'])
+    fields['grade'] = get_grade_dropdown(default=defaults['grade'])
+
+    evals_disc = db(Avaliacoes.disciplina_id == disc_id)
+
     disc = db(db.disciplinas.id==disc_id).select().first()
-    evals_disc = get_evals(None,disc_id)
     disc_grade = grade_average(evals_disc)
     #Lista de avaliações
-    raw_evals = evals_disc.select(limitby=limitby)
+    raw_evals = result_query.select(limitby=limitby)
     evals = refine_evals(raw_evals)
     #Lista de professores que dão a disciplina
     raw_profs = db(db.profs_discs.disciplina_id==disc_id).select()
@@ -45,5 +55,5 @@ def home():
             prof['grade'] = grade_average(evals_prof_disc) 
         profs.append(prof)
 
-    return dict(disc = disc, disc_grade = disc_grade, evals = sorted(evals, key=itemgetter('karma'), reverse=True),\
+    return dict(disc = disc, disc_grade = disc_grade, evals = sorted(evals, key=itemgetter('karma'), reverse=True), fields=fields,
             page=page, per_page=10, profs = sorted(sorted(profs, key=lambda x: rem_acentos(x['full_name'])), key=lambda x: x['grade'], reverse=False))
--- models/helpers/filter_helpers.py
+++ models/helpers/filter_helpers.py
@@ -31,3 +31,31 @@ def get_grade_dropdown(default=None):
                 x.attributes['_selected'] = False
         grade_drop[0].attributes['_selected'] = 'selected'
     return grade_drop
+
+def get_filter_query(query):
+    '''
+    Retorna uma query baseada nos request.vars
+    e os defaults como um dict
+    '''
+    defaults = {
+            'prof_id': None,
+            'disc_id': None,
+            'year'   : None,
+            'grade'  : None
+            }
+
+    if 'prof_id' in request.vars and request.vars['prof_id']:
+        query = query(Avaliacoes.professor_id==request.vars['prof_id'])
+        defaults['prof_id'] = request.vars['prof_id']
+    if 'disc_id' in request.vars and request.vars['disc_id']:
+        query = query(Avaliacoes.disciplina_id==request.vars['disc_id'])
+        defaults['disc_id'] = request.vars['disc_id']
+    if 'year' in request.vars and request.vars['year']:
+        query = query(Avaliacoes.year==request.vars['year'])
+        defaults['year'] = request.vars['year']
+    if 'grade' in request.vars and request.vars['grade']:
+        query = query(Avaliacoes.grade==request.vars['grade'])
+        defaults['grade'] = request.vars['grade']
+
+    return query, defaults
+
