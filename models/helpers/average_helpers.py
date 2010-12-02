ed777a083545cb77701243416d0f4ce154db982e
Tiago <tiago.strato@gmail.com>
2010-12-01 23:40:53 -0200

    Funcoes para calculo de fator de qualidade de aluno

--- models/helpers/average_helpers.py
+++ models/helpers/average_helpers.py
@@ -27,7 +27,7 @@ def get_karma_count_avg():
     counts = map(lambda eval: len(get_karma_list(eval.karmas.select())), allevals)
     counts.sort()
     cut_counts = counts[len(counts)/10:len(counts)-len(counts)/10]
-    return sum(cut_counts)/float(len(cut_counts))
+    return max(1,sum(cut_counts)/float(len(cut_counts)))
 
 def get_avg_positive_rate():
     '''
@@ -60,8 +60,58 @@ def get_eval_quality(eval_row):
 #                aluno quality getters                    #
 ###########################################################
 
+def get_aluno_karmas(aluno_row):
+    '''
+    Gets all karmas received by the user's evaluations
+    '''
+    evals = aluno_row.avaliacoes.select()
+    karmas_lists = map(lambda eval: get_karma_list(eval.karmas.select()), evals)
+    allkarmas = sum(karmas_lists, []) #kids, do NOT try this at home!
+    return allkarmas
+
+def get_aluno_karma_count_avg():
+    '''
+    Gets the treated average student karma count
+    '''
+    allunos = db(Alunos.id>0).select()
+    counts = map(lambda aluno: sum(get_aluno_karmas(aluno)), allunos)
+    counts.sort()
+    cut_counts = counts[len(counts)/10:len(counts)-len(counts)/10]
+    return max(1,sum(cut_counts)/len(cut_counts))
+
+def get_aluno_positive_rate(aluno_row):
+    '''
+    Gets the positive karma rate for a given student
+    '''
+    evals = aluno_row.avaliacoes.select()
+    if len(evals) == 0:
+        return 0
+    pos_rates = map(lambda eval: get_positive_rate(get_karma_list(eval.karmas.select())), evals)
+    return sum(pos_rates)/float(len(pos_rates))
+
+def get_aluno_avg_positive_rate():
+    '''
+    Returns the average positive karma rate of all students
+    '''
+    allunos = db(Alunos.id>0).select()
+    alunos_pos_rates = map(lambda aluno: get_aluno_positive_rate(aluno), allunos)
+    if len(alunos_pos_rates) == 0:
+        return 0
+    pos_avg = sum(alunos_pos_rates)/float(len(alunos_pos_rates))
+    return pos_avg
+
 def get_aluno_quality(aluno_row):
     '''
     Returns the quality index [0-2] for the given user
     '''
-    pass
+    karmas = get_aluno_karmas(aluno_row)
+    if len(karmas) < get_aluno_karma_count_avg():
+        return 1
+    positive_rate = get_aluno_positive_rate(aluno_row)
+    avg_positive_rate = get_aluno_avg_positive_rate()
+    if positive_rate < avg_positive_rate/2.0:
+        return 0
+    elif positive_rate > avg_positive_rate*2.0:
+        return 2
+    else:
+        return 1
