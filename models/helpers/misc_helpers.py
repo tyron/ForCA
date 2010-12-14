59cf45b9ea24e70e762a0402a1b920b7393076e5
Tiago <tiago.strato@gmail.com>
2010-12-13 22:11:54 -0200

    Criado misc_helpers

---
+++ models/helpers/misc_helpers.py
@@ -0,0 +1,12 @@
+def get_contributors():
+    '''
+    Retorna uma lista com os e-mails dos colaboradores do ForCA
+    '''
+    contributors = [
+            'trsilva@inf.ufrgs.br',
+            'adoliveira@inf.ufrgs.br',
+            'tsrodrigues@inf.ufrgs.br',
+            'bomarques@inf.ufrgs.br',
+            'mosilva@inf.ufrgs.br'
+            ]
+    return contributors
